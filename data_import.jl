ce6e935e9aa1d87275062be100de550717dea2e5
J.Bravo <joepbravo@gmail.com>
2021-05-12 00:52:21 -0700

    First Julia script to import data for exploration.

---
+++ data_import.jl
@@ -0,0 +1,7 @@
+import DataFrames
+using DataFrames
+using CSV
+
+dataframe1 = DataFrame( hwid = Int64[],idx = Int32[],x = Float64[],y = Float64[],z = Float64[],r = Float64[],eta = Float64[],phi = Float64[],raw = Float64[],pid = Int[],n = Int32[],truth_eta = Float64[],truth_phi = Float64[],truth_pt = Float64[],trk_good = Float64[],trk_barcode = Float64[],trk_pt = Float64[])
+
+df2 = DataFrame(CSV.File("S:/STAT 196K/Final Project/data_samples/ff3bfb52-f56f-47dd-8dab-0b658e171af5_nevts1_evtid00000140_graphcnn_4l_0j.csv", header = false ))
